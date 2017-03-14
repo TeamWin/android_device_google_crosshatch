22e6cd3e26663581b3f7a5eb936834aa93d7bda8
Steven Moreland <smoreland@google.com>
2017-03-14 00:24:54 -0700

    Remove obsolete ENABLE_TREBLE references.
    ENABLE_TREBLE compiler flags are no longer used. They have been replaced
    with use of the VINTF object.
    
    Bug: 32978887
    Test: pass
    Change-Id: Ic3df7f158eb8cad854f403b7f160e51a25c0b930

--- device-common.mk
+++ device-common.mk
@@ -192,11 +192,6 @@ PRODUCT_PACKAGES += \
   android.hardware.bluetooth@1.0-impl    \
   android.hardware.bluetooth@1.0-service
 
-ifeq ($(ENABLE_TREBLE), true)
-PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-service
-endif
-
 PRODUCT_PACKAGES += \
     libmm-omxcore \
     libOmxCore \
