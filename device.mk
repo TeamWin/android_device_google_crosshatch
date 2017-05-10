d0008ffd5b405b4786dc36f340c843ba64780d58
Chris Fries <cfries@google.com>
2017-05-10 09:57:48 -0500

    device.mk: media_profiles.xml has moved
    Bug: 37956925
    Change-Id: Ibefc7ed2054da66e3b481354854de089ff4c0f93

--- device.mk
+++ device.mk
@@ -398,7 +398,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/media_profiles_V1_0.xml:vendor/etc/media_profiles_V1_0.xml \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.snd_card.open.retries=50
