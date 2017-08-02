c6b1de7d4b3e4221b101208944de2e9ca02f0e9f
Justin Yun <justinyun@google.com>
2017-08-02 17:33:05 +0900

    vndk: remove hidl vndk-sp libs from the list.
    With BOARD_VNDK_VERSION:=current, vndk-sp libs will be installed to
    /system/lib(64)/vndk-sp directory with
    'vndk: {enabled: true, support_system_process: true}' property in
    Android.bp of the module.
    
    Remove the modules that already has the 'vndk' property in their
    Android.bp file in case BOARD_VNDK_VERSION is defined.
    
    Bug: 63866913
    Test: build and boot with and without OARD_VNDK_VERSION=current
    Change-Id: I2718c4940bd72773e97d81e1d78923c8435fa8d6

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -5,8 +5,6 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0\
     android.hardware.graphics.mapper@2.0\
     android.hardware.graphics.common@1.0\
-    libhidlbase\
-    libhidltransport\
     libRS_internal\
     libRSDriver\
     libRSCpuRef\
@@ -28,6 +26,8 @@ VNDK_SP_LIBRARIES += \
     libbacktrace\
     libunwind\
     liblzma\
+    libhidlbase\
+    libhidltransport\
 
 endif
 
