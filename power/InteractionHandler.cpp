2e82cbeaf8071fabf79f9ca332666e3cfdb58e89
Robb Glasser <rglasser@google.com>
2017-12-04 17:28:08 -0800

    Enable idle_state mechanism for b1c1
    Allows us to check the idle state of the display in order to use perf
    hints to improve system performance.
    
    Bug: 69270928
    Bug: 69915886
    
    Test: Booted and cat-ed the affected files
    Change-Id: I8717093e9b8ed9ed35b6ca51a20225abbf71f775

--- power/InteractionHandler.cpp
+++ power/InteractionHandler.cpp
@@ -34,7 +34,7 @@
 #include "hint-data.h"
 #include "utils.h"
 
-#define FB_IDLE_PATH "/sys/class/graphics/fb0/idle_state"
+#define FB_IDLE_PATH "/sys/class/drm/card0/device/idle_state"
 #define MAX_LENGTH 64
 
 #define MSINSEC 1000L
