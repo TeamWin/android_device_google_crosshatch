0d474317e16369b5cb48a85769ec03161b4a43a6
Chris Fries <cfries@google.com>
2018-01-20 21:34:02 -0600

    Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Bug: 72262326
    Test: Boot to SetupWizard
    Change-Id: Ibb6652b022cff2d356b898c2b7b911aeb31262d1

--- device.mk
+++ device.mk
@@ -421,7 +421,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
     android.hardware.audio@2.0-service
 
 # stereo speakers: orientation changes swap L/R channels
