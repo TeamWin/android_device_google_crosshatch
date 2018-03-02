5317934a72db557a3c3dad5d2e96a985cd255052
Chris Fries <cfries@google.com>
2018-03-02 11:01:01 -0600

    TARGET_NO_BOOTLOADER := true
    Simplify the BoardConfig files a little by just setting this true.
    
    Change-Id: I77ff89fa91cec96c57bbad55c527e89fde7de0b8

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -17,6 +17,7 @@
 TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_INFO_FILE := device/google/crosshatch/board-info.txt
 USES_DEVICE_GOOGLE_B1C1 := true
+TARGET_NO_BOOTLOADER := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
