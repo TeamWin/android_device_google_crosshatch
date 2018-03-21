90af3c7b34f2af1f1f4f8bb53687cad9f66d73e0
David Lin <dtwlin@google.com>
2018-03-21 16:18:25 -0700

    haptics: load calibration file for click comp support
    This CL adds the support of per-device calibration file loading for
    click compensation support.
    
    Bug: 70630396
    Test: verify with a dummy calibration pattern
    
    Change-Id: I7fa9787f6bc1bd5cd8629500a1b6a28a6195e419
    Signed-off-by: David Lin <dtwlin@google.com>

--- vibrator/service.cpp
+++ vibrator/service.cpp
@@ -35,6 +35,77 @@ static constexpr char STATE_PATH[] = "/sys/class/leds/vibrator/state";
 static constexpr char EFFECT_INDEX_PATH[] = "/sys/class/leds/vibrator/device/cp_trigger_index";
 static constexpr char DIGI_SCALE_PATH[] = "/sys/class/leds/vibrator/device/dig_scale";
 
+// File path to the calibration file
+static constexpr char CALIBRATION_FILEPATH[] = "/persist/haptics/cs40l20.cal";
+
+// Kernel ABIs for updating the calibration data
+static constexpr char F0_CONFIG[] = "f0_measured";
+static constexpr char REDC_CONFIG[] = "redc_measured";
+static constexpr char F0_FILEPATH[] = "/sys/class/leds/vibrator/device/f0_stored";
+static constexpr char REDC_FILEPATH[] = "/sys/class/leds/vibrator/device/redc_stored";
+
+
+static std::string trim(const std::string& str,
+        const std::string& whitespace = " \t") {
+    const auto str_begin = str.find_first_not_of(whitespace);
+    if (str_begin == std::string::npos) {
+        return "";
+    }
+
+    const auto str_end = str.find_last_not_of(whitespace);
+    const auto str_range = str_end - str_begin + 1;
+
+    return str.substr(str_begin, str_range);
+}
+
+static bool loadCalibrationData() {
+    std::map<std::string, std::string> config_data;
+
+    std::ofstream f0{F0_FILEPATH};
+    if (!f0) {
+        ALOGE("Failed to open %s (%d): %s", F0_FILEPATH, errno,
+                strerror(errno));
+    }
+
+    std::ofstream redc{REDC_FILEPATH};
+    if (!redc) {
+        ALOGE("Failed to open %s (%d): %s", REDC_FILEPATH, errno,
+                strerror(errno));
+    }
+
+    std::ifstream cal_data{CALIBRATION_FILEPATH};
+    if (!cal_data) {
+        ALOGE("Failed to open %s (%d): %s", CALIBRATION_FILEPATH, errno,
+                strerror(errno));
+        return false;
+    }
+
+    for (std::string line; std::getline(cal_data, line);) {
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+        std::istringstream is_line(line);
+        std::string key;
+        if (std::getline(is_line, key, ':')) {
+            std::string value;
+
+            if (std::getline(is_line, value)) {
+                config_data[trim(key)] = trim(value);
+            }
+        }
+    }
+
+    if(config_data.find(F0_CONFIG) != config_data.end()) {
+        f0 << config_data[F0_CONFIG] << std::endl;
+    }
+
+    if(config_data.find(REDC_CONFIG) != config_data.end()) {
+        redc << config_data[REDC_CONFIG] << std::endl;
+    }
+
+    return true;
+}
+
 status_t registerVibratorService() {
     // ostreams below are required
     std::ofstream activate{ACTIVATE_PATH};
@@ -65,6 +136,10 @@ status_t registerVibratorService() {
     state << 1 << std::endl;
     if (!state) {
         ALOGE("Failed to set state (%d): %s", errno, strerror(errno));
+    }
+
+    if (!loadCalibrationData()) {
+        ALOGW("Failed to load calibration data");
     }
 
     sp<IVibrator> vibrator = new Vibrator(std::move(activate), std::move(duration),
