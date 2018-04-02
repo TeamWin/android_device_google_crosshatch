2987c5dcdb282a2db8c573cb93831133608217f5
Yifan Hong <elsk@google.com>
2018-04-02 14:56:59 -0700

    Add crosshatch health impl
    Bug: 74057048 crosshatch health impl
    Bug: 77491619 crash when sysfs nodes are missing
    Bug: 77661605 health service write battery level messages
    Test: lshal debug android.hardware.health@2.0::IHealth/default
    
    Change-Id: Ie68ae8c253c2a7a31da922dcea1fde5e8d21028a

--- device.mk
+++ device.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.crosshatch
 
 # Light HAL
 PRODUCT_PACKAGES += \
