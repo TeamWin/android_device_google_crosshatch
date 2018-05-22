b419c2022c83c7d7ba3fbb29e55595d6d180eeb4
juyuchen <juyuchen@google.com>
2018-05-22 11:57:59 +0800

    audio: update cirrus tuning profile
    Bug: 80106497
    Test: manual audio test on B1/C1
    
    Signed-off-by: juyuchen <juyuchen@google.com>

--- audio/generate_bin.sh
+++ audio/generate_bin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # B1
-python3.5 convert_config_string.py txt 20180312_Blueline_Multimedia_24bit_MediumVibration.h crus_sp_config_b1_rx.bin crus_sp_config_b1_tx.bin
+python3.5 convert_config_string.py txt 20180509_100_Blueline_Stereo_Protect.h crus_sp_config_b1_rx.bin crus_sp_config_b1_tx.bin
 
 # C1
-python3.5 convert_config_string.py txt 20180312_Crosshatch_Multimedia_24bit_MediumVibration.h crus_sp_config_c1_rx.bin crus_sp_config_c1_tx.bin
+python3.5 convert_config_string.py txt 20180509_Crosshatch_Protect_24bit.h crus_sp_config_c1_rx.bin crus_sp_config_c1_tx.bin
