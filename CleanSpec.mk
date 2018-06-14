66300ea16ae332124dc29991661ae48d70476727
Ruchi Kandoi <kandoiruchi@google.com>
2018-06-13 17:09:49 -0700

    Start ISecureElement interface for eSE only on Japan sku
    Test: check for services on respective skus
    Bug: 110053681
    Change-Id: Iede80c1f11d4d3a22f8d148afcc94886671c2515

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -115,3 +115,7 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/product)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/include)
+
+# Secure_element HAL for eSE1
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/etc/init/android.hardware.secure_element@1.0-service.rc)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/vendor/bin/hw/android.hardware.secure_element@1.0-service)
