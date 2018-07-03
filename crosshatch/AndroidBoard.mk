d8dc71002ac0b6260919daeb86e904662f54df1d
Robin Peng <robinpeng@google.com>
2018-07-03 18:45:04 +0800

    Create folder /vendor/firmware_mnt for firmware mount point
    Bug: 110913895
    Change-Id: I2db6fa72edfcbcd976f037eafb09b6355884bb62

--- crosshatch/AndroidBoard.mk
+++ crosshatch/AndroidBoard.mk
@@ -1,5 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+#A/B builds require us to create the mount points at compile time.
+#Just creating it for all cases since it does not hurt.
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT)
+
+$(FIRMWARE_MOUNT_POINT):
+	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
+
 #----------------------------------------------------------------------
 # Generate persist image (persist.img)
 #----------------------------------------------------------------------
