805edb72d0bca42c59421440a896829e9a30c574
Hridya Valsaraju <hridya@google.com>
2018-08-21 21:22:37 -0700

    Build bootctrl.sdm845 using Soong
    This is required to build the recovery variant of the
    same to be used by fastbootd.
    
    Bug: 78793464
    Test: make
    Change-Id: I16eddcb853bda1a9969810f258884ebdbbfc1f5c

--- device.mk
+++ device.mk
@@ -140,7 +140,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.sys.sdcardfs=1
 
 PRODUCT_PACKAGES += \
-    bootctrl.sdm845
+    bootctrl.sdm845 \
+    bootctrl.sdm845.recovery
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.cp_system_other_odex=1
@@ -163,14 +164,6 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-# Enable update engine sideloading by including the static version of the
-# boot_control HAL and its dependencies.
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sdm845 \
-    libgptutils.crosshatch.recovery \
-    libz \
-    libcutils
 
 PRODUCT_PACKAGES += \
     update_engine_sideload \
@@ -451,6 +444,7 @@ PRODUCT_PACKAGES += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
     android.hardware.boot@1.0-service \
 
 # Vibrator HAL
