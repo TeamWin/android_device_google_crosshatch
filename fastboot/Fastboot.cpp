192514ec60455f416ce4dea2a893ba91075a810d
Hridya Valsaraju <hridya@google.com>
2018-09-14 15:38:13 -0700

    Allow fastboot OEM commands to be implemented
    Bug: 78793464
    Bug: 79480454
    Test: fastboot oem setbrightness <level>
    
    Change-Id: Idf6214620a7b7169fe67289ee7d4658f3cfb07da

--- fastboot/Fastboot.cpp
+++ fastboot/Fastboot.cpp
@@ -16,11 +16,24 @@
 
 #include "fastboot/Fastboot.h"
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
 namespace android {
 namespace hardware {
 namespace fastboot {
 namespace V1_0 {
 namespace implementation {
+
+constexpr const char* BRIGHTNESS_FILE = "/sys/class/backlight/panel0-backlight/brightness";
+constexpr int DISPLAY_BRIGHTNESS_DIM_THRESHOLD = 20;
+
+using  OEMCommandHandler = std::function<Result(const std::vector<std::string>&)>;
 
 Return<void> Fastboot::getPartitionType(const ::android::hardware::hidl_string& partitionName,
                                   getPartitionType_cb _hidl_cb) {
@@ -31,6 +44,52 @@ Return<void> Fastboot::getPartitionType(const ::android::hardware::hidl_string& 
         // For bluecross devices, all valid physical partitions need to return raw.
         _hidl_cb(FileSystemType::RAW, { Status::SUCCESS, "" });
     }
+    return Void();
+}
+
+Result SetBrightnessLevel(const std::vector<std::string>& args) {
+    if (!args.size()) {
+        return { Status::INVALID_ARGUMENT, "Brightness level unspecified" };
+    }
+
+    auto level = std::stoi(args[0]);
+
+    if (level < 0 || level > 100)  {
+        return { Status::INVALID_ARGUMENT, "Brighness level must be between 0 and 100" };
+    }
+
+    // Avoid screen being dimmed too much.
+    if (level < DISPLAY_BRIGHTNESS_DIM_THRESHOLD) {
+        level = DISPLAY_BRIGHTNESS_DIM_THRESHOLD;
+    }
+
+    if (android::base::WriteStringToFile(std::to_string(level), BRIGHTNESS_FILE)) {
+            return { Status::SUCCESS, "" };
+    }
+
+    return { Status::FAILURE_UNKNOWN, "Unable to set display brightness" };
+}
+
+Return<void> Fastboot::doOemCommand(const ::android::hardware::hidl_string& oemCmdArgs,
+                          doOemCommand_cb _hidl_cb) {
+    const std::unordered_map<std::string, OEMCommandHandler> kOEMCmdMap = {
+        {FB_OEM_SET_BRIGHTNESS, SetBrightnessLevel},
+    };
+
+    auto args = android::base::Split(oemCmdArgs, " ");
+    if (args.size() < 2) {
+        _hidl_cb({ Status::INVALID_ARGUMENT, "Invalid OEM command" });
+        return Void();
+    }
+
+    // args[0] will be "oem", args[1] will be the command name
+    auto cmd_handler = kOEMCmdMap.find(args[1]);
+    if (cmd_handler != kOEMCmdMap.end()) {
+        _hidl_cb(cmd_handler->second(std::vector<std::string>(args.begin() + 2, args.end())));
+    } else {
+        _hidl_cb({ Status::FAILURE_UNKNOWN, "Unknown OEM command" });
+    }
+
     return Void();
 }
 
