f63bb02f867fbad70de69467edb20ce3a7c896ce
Siarhei Vishniakou <svv@google.com>
2018-10-12 14:41:40 -0700

    Move input configuration files to /vendor
    Currently, these are sitting in the /system partition, which violates
    treble policies.
    Recently, aosp/782082 enabled these for access by system_server.
    
    Bug: 112880217
    Test: dumpsys input, then check to ensure that input files for
    uinput-fpc devices are located in the /vendor partition
    
    Change-Id: I74d3e9023c3a5607f390160d53ba02fd453e7a27

--- device.mk
+++ device.mk
@@ -83,8 +83,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.hardware.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
     $(LOCAL_PATH)/init.power.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_PLATFORM).power.rc \
     $(LOCAL_PATH)/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh \
-    $(LOCAL_PATH)/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/init.qcom.devstart.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.devstart.sh \
     $(LOCAL_PATH)/init.qcom.ipastart.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.ipastart.sh \
     $(LOCAL_PATH)/init.qcom.wlan.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.wlan.sh \
