7517364e97cb1cda014281df6780facc1d29bc02
Daniel Mentz <danielmentz@google.com>
2018-10-12 18:40:14 -0700

    Revert "Add kernel modules to vendor partition"
    This reverts commit 735ff5f42dd319cb6e767136fc1e22c365c34e49.
    
    This should not be required. Installing kernel modules is taken care of
    by setting BOARD_VENDOR_KERNEL_MODULES in BoardConfig-common.mk
    
    Test: treehugger
    Change-Id: I6db1eb4f7320486d63087cb6e007377c4a25754c
    Merged-In: I6db1eb4f7320486d63087cb6e007377c4a25754c
    (cherry picked from commit 1b9c9a68d24d684e7798f8693629f1d6978e1de1)

--- crosshatch/AndroidBoard.mk
+++ crosshatch/AndroidBoard.mk
@@ -33,22 +33,10 @@ ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_PERSISTIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_PERSISTIMAGE_TARGET)
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_PERSISTIMAGE_TARGET)
 
-
-INTERNAL_KERNEL_MODULES := \
-	$(foreach file,$(wildcard device/google/crosshatch-kernel/*.ko \
-	    device/google/crosshatch-kernel/modules.dep),\
-	    $(file):$(TARGET_OUT_VENDOR)/lib/modules/$(notdir $(file)))
-
-INSTALLED_KERNEL_MODULES := $(call copy-many-files,$(INTERNAL_KERNEL_MODULES))
-
-.PHONY: kernel-modules
-kernel-modules: $(INSTALLED_KERNEL_MODULES)
-	@echo Kernel modules installed
-
 .PHONY: persistimage
 persistimage: $(INSTALLED_PERSISTIMAGE_TARGET)
 
-droidcore: $(INSTALLED_PERSISTIMAGE_TARGET) $(INSTALLED_KERNEL_MODULES)
+droidcore: $(INSTALLED_PERSISTIMAGE_TARGET)
 
 
 # copy kernel headers to the build tree
