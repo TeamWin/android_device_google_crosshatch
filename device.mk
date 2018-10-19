fee06f72c7eef5f839177823cc60a11b93f48016
Michele Berionne <mberionne@google.com>
2018-10-19 16:29:09 -0700

    Added persist.vendor.radio.sap_silent_pin property set to 1.
    This property controls if the user has to enter the PIN code manually
    after disconnecting the phone from the car using the bluetooth SAP
    profile. When SAP profile is used, it's equivalent as moving the SIM
    card from the phone into the car, and thus the default behavior is to
    request the PIN code from the user. Anyway, this is usually annoying for
    the user, as he might not check the screen and suddenly becomes
    unreachable.
    
    Test: adb shell getprop | grep "sap_silent_pin"
    Bug: 117307506
    Change-Id: I69ff0ea1c74461fd1fb19a7b3ee71b083ea05de5

--- device.mk
+++ device.mk
@@ -297,10 +297,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.videopause.mode=1 \
     persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.data_con_rprt=true \
-    persist.vendor.radio.relay_oprt_change=1\
-    persist.vendor.radio.no_wait_for_card=1\
+    persist.vendor.radio.relay_oprt_change=1 \
+    persist.vendor.radio.no_wait_for_card=1 \
+    persist.vendor.radio.sap_silent_pin=1 \
     persist.rcs.supported=1 \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so\
+    vendor.rild.libpath=/vendor/lib64/libril-qc-hal-qmi.so \
     ro.hardware.keystore_desede=true \
 
 # Disable snapshot timer
