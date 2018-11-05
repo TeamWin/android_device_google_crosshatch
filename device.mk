2e2ff45f44eb5b5e99404309a377b3a34390b65f
Jeff Tinker <jtinker@google.com>
2018-11-05 15:01:48 -0800

    Enable clearkey drm plugin v1.2 on crosshatch
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: I86fedc9d383f90cf874c8001f7064b9bfbbe3658

--- device.mk
+++ device.mk
@@ -368,7 +368,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.2-service.clearkey \
     android.hardware.drm@1.1-service.widevine
 
 # NFC and Secure Element packages
