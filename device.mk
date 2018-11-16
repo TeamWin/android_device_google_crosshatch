aea71ac1b7d078dd6873be4e97bd3581ed327b1d
Jack Yu <jackcwyu@google.com>
2018-11-16 18:25:24 +0800

    [DO NOT MERGE] Remove loaderservice scripts from release image
    Short-term workaround to recover felica functionality on fail device.
    Also lower the risk for executing loader service update fail.
    
    Bug:115768202
    Test: Build pass and loader service update wont start
    Change-Id: If5368f81c117419db5084709cd1dfe1537f64246

--- device.mk
+++ device.mk
@@ -388,9 +388,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     device/google/crosshatch/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
-    device/google/crosshatch/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf \
-    device/google/crosshatch/felica/loaderservice_updater_1.lss:$(TARGET_COPY_OUT_VENDOR)/etc/loaderservice_updater_1.lss \
-    device/google/crosshatch/felica/loaderservice_updater_2.lss:$(TARGET_COPY_OUT_VENDOR)/etc/loaderservice_updater_2.lss
+    device/google/crosshatch/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf
 
 # TODO(b/72443662)
 PRODUCT_COPY_FILES += \
