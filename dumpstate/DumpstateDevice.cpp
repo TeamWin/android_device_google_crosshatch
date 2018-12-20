f43792df6e92c3c730fbf6cd529b2e11c20f9fe2
Willy Hu <willycwhu@google.com>
2018-12-20 10:34:30 +0800

    ril: Support 2nd RIL Logging
    Support 2nd RIL to Logging logs into ril_log1.
    
    Bug: 122514862
    Test: local test pass, we can see 1st ril logging log into ril_log0
    and 2nd ril to logging log into ril_log1. It can collect the ril_log0
    /ril_log1 logs successfully with bugreport.
    
    Change-Id: I9b3a58accb5ea4b4754ab16865853463e451a364

--- dumpstate/DumpstateDevice.cpp
+++ dumpstate/DumpstateDevice.cpp
@@ -133,8 +133,10 @@ void DumpstateDevice::dumpModem(int fd, int fdModem)
         const std::string diagLogDir = "/data/vendor/radio/diag_logs/logs";
         const std::string tcpdumpLogDir = "/data/vendor/tcpdump_logger/logs";
         const std::vector<std::string> rilAndNetmgrLogs{
-            "/data/vendor/radio/ril_log",
-            "/data/vendor/radio/ril_log_old",
+            "/data/vendor/radio/ril_log0",
+            "/data/vendor/radio/ril_log0_old",
+            "/data/vendor/radio/ril_log1",
+            "/data/vendor/radio/ril_log1_old",
             "/data/vendor/radio/imsdatadaemon_log",
             "/data/vendor/radio/imsdatadaemon_log_old",
             "/data/vendor/radio/qmi_fw_log",
