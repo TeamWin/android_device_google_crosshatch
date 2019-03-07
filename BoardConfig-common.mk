05e6221c7a53bb9a95b1b0d55594b8a3ee460546
Andreas Gampe <agampe@google.com>
2019-03-07 10:45:45 -0800

    Crosshatch: Custom modules pickup
    When creating a product with TARGET_PREBUILT_KERNEL, allow
    to pick up modules that match the given kernel by looking
    for .ko and .dtb files in the same directory. This avoids
    further special-casing in the board config.
    
    Bug: 128324084
    Test: m
    Test: manual
    Merged-In: I6227d422650ddc39d5da21c3e76f50875217d811
    Change-Id: I6227d422650ddc39d5da21c3e76f50875217d811

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -255,6 +255,17 @@ BOARD_VENDOR_KERNEL_MODULES += \
 else ifeq (,$(filter-out blueline_kernel_debug_api crosshatch_kernel_debug_api, $(TARGET_PRODUCT)))
 BOARD_VENDOR_KERNEL_MODULES += \
     $(wildcard device/google/crosshatch-kernel/debug_api/*.ko)
+else ifneq (,$(TARGET_PREBUILT_KERNEL))
+    # If TARGET_PREBUILT_KERNEL is set, check whether there are modules packaged with that kernel
+    # image. If so, use them, otherwise fall back to the default directory.
+    TARGET_PREBUILT_KERNEL_PREBUILT_VENDOR_KERNEL_MODULES := \
+        $(wildcard $(dir $(TARGET_PREBUILT_KERNEL))/*.ko)
+    ifneq (,$(TARGET_PREBUILT_KERNEL_PREBUILT_VENDOR_KERNEL_MODULES))
+        BOARD_VENDOR_KERNEL_MODULES += $(TARGET_PREBUILT_KERNEL_PREBUILT_VENDOR_KERNEL_MODULES)
+    else
+        BOARD_VENDOR_KERNEL_MODULES += $(wildcard device/google/crosshatch-kernel/*.ko)
+    endif
+    # Do NOT delete TARGET_PREBUILT..., it will lead to empty BOARD_VENDOR_KERNEL_MODULES.
 else
 BOARD_VENDOR_KERNEL_MODULES += \
     $(wildcard device/google/crosshatch-kernel/*.ko)
