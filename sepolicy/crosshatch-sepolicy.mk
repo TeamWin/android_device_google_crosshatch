be321e54bcb91a38dd67fed5546a9d8c45efc2d2
bigbiff <bigbiff@teamw.in>
2019-04-13 13:08:44 +0000

    Add sepolicy from lineageos.

---
+++ sepolicy/crosshatch-sepolicy.mk
@@ -0,0 +1,8 @@
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := device/google/crosshatch/sepolicy/public
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/google/crosshatch/sepolicy/private
+
+# vendors
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/qcom/common
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/qcom/sdm845
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/google
+BOARD_SEPOLICY_DIRS += device/google/crosshatch/sepolicy/vendor/verizon
