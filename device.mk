6c50d282fb9957049960aca12932f481036124e1
Paul Scovanner <pscovanner@google.com>
2019-06-11 12:52:34 -0700

    [DO NOT MERGE] Update Crosshatch SVN to 12 Bug:126590667

--- device.mk
+++ device.mk
@@ -784,4 +784,4 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=11
+    ro.vendor.build.svn=12
