96ce3928cffc93460a07d83de7815028a5bab5ce
Paul Scovanner <pscovanner@google.com>
2019-08-14 14:02:40 -0700

    Update Crosshatch SVN to 17
    Bug:139434268
    Change-Id: Ib75c9aa1e4baf969bc1db40020b38c5462f87bf3
    (cherry picked from commit ac94d5c43962507d140b4397662d2673325749c5)

--- device.mk
+++ device.mk
@@ -816,7 +816,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=16
+    ro.vendor.build.svn=17
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
