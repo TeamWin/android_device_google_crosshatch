3ab535592d75edeed6505869c081d87fe88e1226
Cheng Gu <gucheng@google.com>
2019-10-23 17:26:52 -0700

    device.mk: Add a camera test library to packages
    Adds libgoogle_camera_hal_proprietary_tests to packages.
    
    Also guards both test libraries to debug builds only.
    
    Bug: 142818926
    Test: Presubmit builds/tests
    Change-Id: I31f107a447ac1c65281f817393c162bad8487dd5

--- device.mk
+++ device.mk
@@ -459,11 +459,17 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     camera.sdm845 \
     libgooglecamerahal \
-    libgoogle_camera_hal_tests \
     libqomx_core \
     libmmjpeg_interface \
     libmmcamera_interface \
     libcameradepthcalibrator
+
+# Google Camera HAL test libraries in debug builds
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+    libgoogle_camera_hal_proprietary_tests \
+    libgoogle_camera_hal_tests
+endif
 
 PRODUCT_PACKAGES += \
     sensors.$(PRODUCT_HARDWARE) \
