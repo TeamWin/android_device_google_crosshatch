76a514cea56c6f47e13443d55c38a3bc02311198
Sasha Smundak <asmundak@google.com>
2019-12-06 12:39:32 -0800

    Remove setting LOCAL_MULTILIB from AUDIOSERVER_MULTILIB
    Bug: 121208203
    Test: treehugger
    Change-Id: I9bfdaa1e5f46e3aee7714f420916f04b25a40b6b

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -19,7 +19,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := voice_processing_descriptors.c
 LOCAL_C_INCLUDES += $(call include-path-for, audio-effects)
 LOCAL_HEADER_LIBRARIES := libhardware_headers
-LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
 LOCAL_MODULE := libqcomvoiceprocessingdescriptors
 LOCAL_MODULE_RELATIVE_PATH := soundfx
 LOCAL_MODULE_TAGS := optional
