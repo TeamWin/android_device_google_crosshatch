d700c7695e044c2614231b04809500f220eef453
Stephen Hines <srhines@google.com>
2019-12-13 15:16:22 -0800

    Add aosp_blueline_hwasan and aosp_crosshatch_hwasan build configs.
    Bug: 145023658
    Test: lunch aosp_blueline_hwasan-userdebug
    Test: lunch aosp_crosshatch_hwasan-userdebug
    Change-Id: Id54f7c77889be7666d55577137a59e0aa9a08062

--- aosp_blueline_hwasan.mk
+++ aosp_blueline_hwasan.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,10 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_crosshatch.mk \
-    $(LOCAL_DIR)/aosp_blueline.mk \
-    $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
-    $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
+$(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
+PRODUCT_NAME := aosp_blueline_hwasan
 
-COMMON_LUNCH_CHOICES := \
-    aosp_crosshatch-userdebug \
-    aosp_blueline-userdebug
+# Add "hwaddress" as a global sanitizer if it's missing.
+ifeq ($(filter hwaddress,$(SANITIZE_TARGET)),)
+  SANITIZE_TARGET := $(strip $(SANITIZE_TARGET) hwaddress)
+endif
