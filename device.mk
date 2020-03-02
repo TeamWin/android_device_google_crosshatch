768bea6065c742ff58380112671538650c9a1c55
Lubo Hristov <lubomir@google.com>
2020-03-02 15:28:36 -0800

    Set crosshatch SVN to 28 bug:149240442

--- device.mk
+++ device.mk
@@ -827,7 +827,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=27
+    ro.vendor.build.svn=28
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
