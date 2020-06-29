9441c0ccaaf618a0acdd8461ad956cadba31df76
Paul Scovanner <pscovanner@google.com>
2020-06-29 18:41:13 +0000

    DO NOT MERGE Update Crosshatch SVN to 35
    Bug: 160158282
    Change-Id: I3b0d25afb62d70d1c90016d3e02aab72644db6c4

--- device.mk
+++ device.mk
@@ -828,7 +828,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=34
+    ro.vendor.build.svn=35
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
