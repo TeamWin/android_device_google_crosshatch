93c68dfc32ea62b5c0026c66ef8bc59d0ea1d664
Paul Scovanner <pscovanner@google.com>
2020-06-29 19:15:35 +0000

    Update Crosshatch SVN to 37 for R
    Bug: 160158282
    Change-Id: Id6ade39db69f99d5c1c4ce0718ce3008439aeaf7

--- device.mk
+++ device.mk
@@ -843,7 +843,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.svn=34
+    ro.vendor.build.svn=37
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.adb.secure=1
