6817e90350171c079162586ac5ed9abcc5a6862e
Felipe Leme <felipeal@google.com>
2020-07-10 16:20:56 -0700

    Add option to use a custom board config through PHONE_CAR_BOARD_CONFIG.
    phone_car variants use the same board as phone, but need to customize
    some properties like screen density.
    
    Test: . ./build/envsetup.sh && lunch blueline_car-eng && m && grep ro.sf.lcd_density $OUT/vendor/build.prop
    
    Bug: 160821620
    Bug: 160823761
    
    Change-Id: I0369251767d97575c482cdb010593de32149b115

--- blueline/BoardConfig.mk
+++ blueline/BoardConfig.mk
@@ -14,8 +14,12 @@
 # limitations under the License.
 #
 
-TARGET_BOOTLOADER_BOARD_NAME := blueline
-TARGET_SCREEN_DENSITY := 440
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 20
+ifdef PHONE_CAR_BOARD_CONFIG
+  include $(PHONE_CAR_BOARD_CONFIG)
+else
+  TARGET_BOOTLOADER_BOARD_NAME := blueline
+  TARGET_SCREEN_DENSITY := 440
+  TARGET_RECOVERY_UI_MARGIN_HEIGHT := 20
 
-include device/google/crosshatch/BoardConfig-common.mk
+  include device/google/crosshatch/BoardConfig-common.mk
+endif
