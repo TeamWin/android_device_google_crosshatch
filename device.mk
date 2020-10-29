34dc3be0c12b8017d3446cce596848dce37f9764
Meng-hsuan Chung <menghsuan@google.com>
2020-10-29 14:46:05 +0000

    Include CHRE tools in device image
    Bug: 171734178
    Test: build and verify tools are included
    Change-Id: I3175cfdad55753128232f8221a51e2e8c57c22ee

--- device.mk
+++ device.mk
@@ -511,6 +511,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.contexthub@1.2-service.generic
 
+# CHRE tools
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+    chre_power_test_client \
+    chre_test_client
+endif
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
