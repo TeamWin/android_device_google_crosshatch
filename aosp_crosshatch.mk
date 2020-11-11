a1ba8e04ce196dba8405d549facd9bb928c74d5e
Raman Tenneti <rtenneti@google.com>
2020-11-11 08:24:53 -0800

    Adding missing entries for PRODUCT_SOONG_NAMESPACES for aosp_crosshatch by running `lunch` aosp_crosshatch-userdebug and crosshatch-userdebug and comparing the PRODUCT_SOONG_NAMESPACES.
    Didn't add the following entries as they seem to be google apps specific.
      vendor/unbundled_google/packages/TurboAdapter
      vendor/google/apps/ExtremeBatterySaver
    
    Bug: 128335791
    
    Test: Manual
    + aosp_crosshatch-userdebug,crosshatch_hwasan-userdebug
      https://android-build.googleplex.com/builds/forrest/run/L93300000741498416
    + aosp_blueline-userdebug
      https://android-build.googleplex.com/builds/forrest/run/L08500000741456703
    
    
    Change-Id: I24299ea2efc67914ddfb140137532838870e27ce

--- aosp_crosshatch.mk
+++ aosp_crosshatch.mk
@@ -44,6 +44,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, device/google/crosshatch/device-crosshatch.mk)
 $(call inherit-product-if-exists, vendor/google_devices/crosshatch/proprietary/device-vendor.mk)
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/google_devices/crosshatch/proprietary/hardwareinfo
+
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
 # STOPSHIP deal with Qualcomm stuff later
