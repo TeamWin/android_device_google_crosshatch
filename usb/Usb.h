889b8edf80c083240e03f67404a4449f3075be32
Albert Wang <albertccwang@google.com>
2020-11-27 16:13:05 +0800

    Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling
    
    Bug: 161414036
    Test: Pass USB V1.3 HIDL tests
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: Ic9c029d03a6642e1f7473b4240fa8857cda23cc4

--- usb/Usb.h
+++ usb/Usb.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <android/hardware/usb/1.2/IUsb.h>
+#include <android-base/file.h>
 #include <android/hardware/usb/1.2/IUsbCallback.h>
 #include <android/hardware/usb/1.2/types.h>
+#include <android/hardware/usb/1.3/IUsb.h>
 #include <hidl/Status.h>
 #include <utils/Log.h>
 
@@ -16,10 +17,11 @@
 namespace android {
 namespace hardware {
 namespace usb {
-namespace V1_2 {
+namespace V1_3 {
 namespace implementation {
 
 using ::android::sp;
+using ::android::base::WriteStringToFile;
 using ::android::hardware::hidl_array;
 using ::android::hardware::hidl_memory;
 using ::android::hardware::hidl_string;
@@ -33,22 +35,28 @@ using ::android::hardware::usb::V1_0::PortRoleType;
 using ::android::hardware::usb::V1_0::Status;
 using ::android::hardware::usb::V1_1::PortMode_1_1;
 using ::android::hardware::usb::V1_1::PortStatus_1_1;
-using ::android::hardware::usb::V1_2::IUsb;
 using ::android::hardware::usb::V1_2::IUsbCallback;
 using ::android::hardware::usb::V1_2::PortStatus;
+using ::android::hardware::usb::V1_3::IUsb;
 using ::android::hidl::base::V1_0::DebugInfo;
 using ::android::hidl::base::V1_0::IBase;
 
-enum class HALVersion { V1_0, V1_1, V1_2 };
+#define SOC_PATH "/sys/devices/platform/soc/a600000.ssusb/"
+#define VBUS_PATH SOC_PATH "id"
+#define ID_PATH SOC_PATH "b_sess"
+#define USB_DATA_PATH SOC_PATH "usb_data_enabled"
+
+enum class HALVersion { V1_0, V1_1, V1_2, V1_3 };
 
 struct Usb : public IUsb {
     Usb();
 
-    Return<void> switchRole(const hidl_string& portName, const V1_0::PortRole& role) override;
+    Return<void> switchRole(const hidl_string &portName, const PortRole &role) override;
     Return<void> setCallback(const sp<V1_0::IUsbCallback>& callback) override;
     Return<void> queryPortStatus() override;
     Return<void> enableContaminantPresenceDetection(const hidl_string &portName, bool enable);
     Return<void> enableContaminantPresenceProtection(const hidl_string &portName, bool enable);
+    Return<bool> enableUsbDataSignal(bool enable) override;
 
     sp<V1_0::IUsbCallback> mCallback_1_0;
     // Protects mCallback variable
@@ -67,7 +75,7 @@ struct Usb : public IUsb {
 };
 
 }  // namespace implementation
-}  // namespace V1_2
+}  // namespace V1_3
 }  // namespace usb
 }  // namespace hardware
 }  // namespace android
